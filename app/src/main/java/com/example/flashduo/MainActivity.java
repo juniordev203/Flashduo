6025d1ade3797b9769d528aacde5774ceaceda3d
Huy Hoàng <ttk11092k3@gmail.com>
2024-03-19 16:00:08 +0700

    hh

--- app/src/main/java/com/example/flashduo/MainActivity.java
+++ app/src/main/java/com/example/flashduo/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.flashduo;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,8 +11,11 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.util.Currency;
+
 public class MainActivity extends AppCompatActivity {
 
+    Button btnbatdau;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,5 +26,14 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        btnbatdau = findViewById(R.id.btnbatdau);
+        btnbatdau.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, chooselanguage.class);
+                startActivity(intent);
+            }
+        });
     }
+
 }
---
+++ app/src/main/java/com/example/flashduo/chooselanguage.java
@@ -0,0 +1,6 @@
+package com.example.flashduo;
+
+import android.app.Activity;
+
+public class chooselanguage extends Activity {
+}
