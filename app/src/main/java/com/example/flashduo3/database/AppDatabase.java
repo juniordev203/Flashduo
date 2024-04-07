de2356d9bd2d111eb699f5c1c748bfa440a234fc
Huy Hoàng <ttk11092k3@gmail.com>
2024-04-07 14:43:57 +0700

    update

---
+++ app/src/main/java/com/example/flashduo3/database/AppDatabase.java
@@ -0,0 +1,25 @@
+package com.example.flashduo3;
+
+import android.content.Context;
+
+import androidx.room.Database;
+import androidx.room.Room;
+import androidx.room.RoomDatabase;
+import androidx.room.TypeConverters;
+
+import com.example.flashduo3.database.WordDao;
+
+@Database(entities = {Word.class}, version = 1)
+@TypeConverters({Converters.class})
+public abstract class AppDatabase extends RoomDatabase {
+    public static AppDatabase db;
+
+    public abstract WordDao wordDao();
+
+    public static AppDatabase getDatabase(final Context context){
+        if (db == null){
+            db = Room.databaseBuilder(context.getApplicationContext(), AppDatabase.class, "word_database").build();
+        }
+        return db;
+    }
+}
--- app/src/main/java/com/example/flashduo3/database/WordDao.java
+++ app/src/main/java/com/example/flashduo3/database/WordDao.java
@@ -1,14 +1,41 @@
 package com.example.flashduo3.database;
 
+import androidx.room.ColumnInfo;
 import androidx.room.Dao;
+import androidx.room.Insert;
+import androidx.room.PrimaryKey;
 import androidx.room.Query;
+import androidx.room.Update;
 
 import com.example.flashduo3.Word;
 
 import java.util.List;
 
+
 @Dao
 public interface WordDao {
-    @Query("SELECT * FROM word")
-    List<Word> getListWord();
+    @Insert
+    void insert(Word word);
+
+    @Insert
+    void insertAll(List<Word> words);
+
+    @Query("Select * FROM Word where id = :wordId")
+    Word getById(int wordId);
+
+    @Query("Select id, chinese, meaning, picture FROM Word where id = :wordId")
+    Word getWordById(int wordId);
+
+    @Query("Select id, question, options, answer FROM Word where id = :questionId")
+    Word getQuestionById(int questionId);
+
+    @Query("SELECT id, sentence FROM Word where id = :sentenceId")
+    Word getSentenceById(int sentenceId);
+
+    @Update
+    void update(Word word);
+
+    @Query("DELETE FROM word")
+    void deleteAll();
+
 }
